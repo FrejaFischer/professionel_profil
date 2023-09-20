7b4459fcb3eb6c6c3ced9f9d58e05e0fe7b2b282
Freja Fischer <frej657e@stud.kea.dk>
2023-09-20 12:54:00 +0200

    cat feature done 4 now

--- scripts.js
+++ scripts.js
@@ -7,6 +7,21 @@ function start() {
   body.dataset.theme = "dark";
   select.value = "dark";
   select.addEventListener("change", changeTheme);
+
+  document.querySelector(".cat").addEventListener("mouseover", () => {
+    // document.querySelector(".cat").style.cursor = "pointer";
+    document.querySelector(".eyeball").classList.add("disappear");
+    document.querySelector(".eyeball").classList.add("hide");
+    document.querySelector(".eyeball_big").classList.remove("hide");
+    document.querySelector(".eyeball_big").classList.add("appear");
+  });
+  document.querySelector(".cat").addEventListener("mouseout", () => {
+    document.querySelector(".eyeball").classList.remove("disappear");
+    document.querySelector(".eyeball").classList.remove("hide");
+    document.querySelector(".eyeball").classList.add("appear");
+    document.querySelector(".eyeball_big").classList.remove("appear");
+    document.querySelector(".eyeball_big").classList.add("hide");
+  });
 }
 
 //Theme switcher
